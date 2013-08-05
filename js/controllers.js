a2f2553a09adde0e942856220370d85a144d597a
Anrong <erik.sparre@gmail.com>
2013-08-05 12:15:14 +0530

    Quantity input moved to product page
    Fixed bugs

--- js/controllers.js
+++ js/controllers.js
@@ -728,11 +728,16 @@ var QuotationCtrl = function($rootScope, $scope){
             return 0;
         }
         if (quote=='1'){
+            console.log("discount one");
             amount = $rootScope.TotalSum()*rate/100;
+            return amount;
         }
-        amount = $rootScope.TotalSum2()*rate/100;
+        else if (quote =='2'){
+            amount = $rootScope.TotalSum2()*rate/100;
+            console.log("discount two");
+            return amount;
+        }
 
-        return amount;
     }
 
     $rootScope.includeDiscount = function (quote){
